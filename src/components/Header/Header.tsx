b7a446d36a00aeabf129e9318e0825bb1e321a42
Dandara Estrela <dandara_estrela@hotmail.com>
2023-03-28 20:58:09 -0300

    feat: creating default layout and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,24 @@
+import * as S from "./Header.styles";
+
+import { Timer, Scroll } from "phosphor-react";
+
+import logoIgnite from "../../assets/Logo.svg";
+import { NavLink } from "react-router-dom";
+
 export function Header() {
-  return <h1>Header</h1>;
+  return (
+    <S.HeaderContainer>
+      <span>
+        <img src={logoIgnite} alt="imagem de logotipo" />
+      </span>
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </S.HeaderContainer>
+  );
 }
